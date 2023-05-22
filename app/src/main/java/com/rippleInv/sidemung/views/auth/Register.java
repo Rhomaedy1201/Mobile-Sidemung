d7c934e35183a878d42862dece7ec6d94f78fd40
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2023-05-22 14:46:24 +0700

    Manifest
    Menambahkan Inernet akses di Manifest

--- app/src/main/java/com/rippleInv/sidemung/views/auth/Register.java
+++ app/src/main/java/com/rippleInv/sidemung/views/auth/Register.java
@@ -31,8 +31,6 @@ public class Register extends AppCompatActivity {
     private String TAG = "ActivityRegister";
     private TextInputEditText email, phone, nik, password, passwordConfirm;
     private Button login;
-
-    private String link = "http://192.168.1.9:8000/api/register";
     private ProgressBar loadingPB;
 
     @Override
@@ -46,8 +44,6 @@ public class Register extends AppCompatActivity {
         password = findViewById(R.id.tv_password);
         passwordConfirm = findViewById(R.id.tv_password_cofirm);
         login = findViewById(R.id.btnToLogin);
-
-        postUser();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,44 +59,39 @@ public class Register extends AppCompatActivity {
         });
     }
 
-    private void postUser(){
-        loadingPB.setVisibility(View.VISIBLE);
-        RequestQueue queue = Volley.newRequestQueue(Register.this);
-        StringRequest request = new StringRequest(Request.Method.POST, LinkApi.Register, new com.android.volley.Response.Listener<String>() {
+    public void postUser(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, LinkApi.Register, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                loadingPB.setVisibility(View.GONE);
-//                nameEdt.setText("");
-//                jobEdt.setText("");
-
-                Toast.makeText(Register.this, "Data added to API", Toast.LENGTH_SHORT).show();
-                try {
-                    JSONObject respObj = new JSONObject(response);
-
-                    String name = respObj.getString("name");
-                    String job = respObj.getString("job");
-
-                    responseTV.setText("Name : " + name + "\n" + "Job : " + job);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+                Log.e("Rest response",response);
             }
-        }, new com.android.volley.Response.ErrorListener() {
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+                Log.e("Rest response",error.toString());
             }
-        }) {
+        }){
             @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("name", name);
-                params.put("job", job);
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("nik", "565766776666");
+                params.put("name", "inputName");
+                params.put("email", "inputEmail@gmail.com");
+                params.put("phone", "085234654123");
+                params.put("password", "inputPassword");
                 return params;
             }
+
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+
         };
-        queue.add(request);
+
+        requestQueue.add(stringRequest);
     }
 }
